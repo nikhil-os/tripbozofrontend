db6834344252f7d94b2ff0c62768d0c0d88ebe00
Suryansh Sharma <suryansharma09@gmail.com>
2025-06-13 16:53:44 +0530

    divided layout

---
+++ components/RootWrapper.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function RootWrapper({ children }) {
+  const pathname = usePathname();
+  const isBundle = pathname?.startsWith("/bundle-redirect");
+
+  return (
+    <>
+      {!isBundle && <Navbar />}
+      {!isBundle && <div className="h-16 w-full" />} {/* spacer */}
+      <main
+        className={
+          isBundle
+            ? "min-h-screen"
+            : "min-h-[calc(100vh-64px-200px)] pb-32"
+        }
+      >
+        {children}
+      </main>
+      {!isBundle && <Footer />}
+    </>
+  );
+}
