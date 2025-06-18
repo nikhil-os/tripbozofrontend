99f6c59cecae69988c2656dd3c6becb0b5d9e550
Suryansh Sharma <suryansharma09@gmail.com>
2025-06-18 14:48:03 +0530

    implementing images api in frontned- prod 4

--- src/app/country/[country]/page.js
+++ src/app/country/[country]/page.js
@@ -1,34 +1,18 @@
-// src/app/country/[country]/page.jsx
+// app/country/[country]/page.jsx
 import CountryAppsPage from "@/components/countryapp/CountryAppsPage";
 import { fetchAppsByCountry, fetchCountryInfo } from "@/src/utils/api";
 import { notFound } from "next/navigation";
 
+
 export default async function CountryPage({ params }) {
   const countryCode = params.country.toUpperCase();
 
-  // 1) Fetch country metadata
-  const countryInfo = await fetchCountryInfo(countryCode);
+  const [countryInfo, apps, images] = await Promise.all([
+    fetchCountryInfo(countryCode),
+    fetchAppsByCountry(countryCode),
+    
+  ]);
 
-  // 2) Fetch apps list
-  const apps = await fetchAppsByCountry(countryCode);
-
-  // 3) Fetch hero images (we call it here, passing countryCode)
-  async function fetchCountryImages(code) {
-    try {
-      const res = await fetch(`/api/country/${code}`);
-      if (!res.ok) {
-        console.error("Country images API returned", res.status);
-        return [];
-      }
-      return await res.json();
-    } catch (err) {
-      console.error("fetchCountryImages failed:", err);
-      return [];
-    }
-  }
-  const images = await fetchCountryImages(countryCode);
-
-  // If no country or no apps, 404:
   if (!countryInfo.name || apps.length === 0) {
     return notFound();
   }
@@ -38,7 +22,7 @@ export default async function CountryPage({ params }) {
       countryCode={countryInfo.code}
       countryInfo={countryInfo}
       apps={apps}
-      heroImages={images}
+      
     />
   );
 }
